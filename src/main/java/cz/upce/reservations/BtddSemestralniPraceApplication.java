ce7c7c04d41819d980f8e7db71424b1f60e5f529
KryxusCZ <st69861@upce.cz>
2026-02-25 11:28:03 +0100

    init: spring boot project setup

---
+++ src/main/java/cz/upce/reservations/BtddSemestralniPraceApplication.java
@@ -0,0 +1,13 @@
+package cz.upce.reservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BtddSemestralniPraceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BtddSemestralniPraceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cz/upce/reservations/BtddSemestralniPraceApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.upce.reservations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BtddSemestralniPraceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
