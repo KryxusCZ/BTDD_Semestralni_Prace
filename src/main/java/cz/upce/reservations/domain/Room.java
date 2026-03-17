87e658a1626a90caf1bb75aebf69555896b26f2d
KryxusCZ <st69861@upce.cz>
2026-03-17 12:20:12 +0100

    feat: add Room entity

---
+++ src/main/java/cz/upce/reservations/domain/Room.java
@@ -0,0 +1,76 @@
+package cz.upce.reservations.domain;
+
+import jakarta.persistence.*;
+import java.time.LocalTime;
+
+@Entity
+@Table(name = "rooms")
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String location;
+    private int capacity;
+    private LocalTime openingTime;
+    private LocalTime closingTime;
+    private boolean active;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public LocalTime getOpeningTime() {
+        return openingTime;
+    }
+
+    public LocalTime getClosingTime() {
+        return closingTime;
+    }
+
+    public boolean isActive() {
+        return active;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
+
+    public void setOpeningTime(LocalTime openingTime) {
+        this.openingTime = openingTime;
+    }
+
+    public void setClosingTime(LocalTime closingTime) {
+        this.closingTime = closingTime;
+    }
+
+    public void setActive(boolean active) {
+        this.active = active;
+    }
+
+    public Room() {}
+}
